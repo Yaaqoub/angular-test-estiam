d21a485e435e2cf893e66e020464ac83fcc061f8
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2023-10-18 12:53:54 +0200

    list users from json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { UserService } from "./user/user.service";
+import { UserViewComponent } from './user/user-view/user-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user/user-view/user-view.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../interfaces/user.interface";
+import {UserService} from "../user.service";
+
+@Component({
+  selector: 'app-user-view',
+  templateUrl: './user-view.component.html',
+  styleUrls: ['./user-view.component.css']
+})
+export class UserViewComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    })
+  }
+}
